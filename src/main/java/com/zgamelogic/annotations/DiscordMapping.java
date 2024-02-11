f386a843ceb5f135d6cb52e8b464258d56ac57ff
Ben Shabowski <benshabowski@gmail.com>
2024-02-11 10:16:38 -0600

    Adding documentation

--- src/main/java/com/zgamelogic/annotations/DiscordMapping.java
+++ src/main/java/com/zgamelogic/annotations/DiscordMapping.java
@@ -14,7 +14,35 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface DiscordMapping {
+
+    /**
+     * Use this to have this method only called when the id of a command/interaction equals this string.
+     * This works with the following
+     * @see net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+     * @see net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
+     * @see net.dv8tion.jda.api.events.interaction.command.MessageContextInteractionEvent
+     * @see net.dv8tion.jda.api.events.interaction.command.UserContextInteractionEvent
+     * @see net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent
+     * @see net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
+     * @see net.dv8tion.jda.api.events.interaction.ModalInteractionEvent
+     * @return id/name of the interaction you would like to match it with
+     */
     String Id() default "";
+
+    /**
+     * Use this to have this method only called when the sub command id of a command equals this string.
+     * This works with the following
+     * @see net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+     * @see net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
+     * @return sub-id of the command you would like to match it with
+     */
     String SubId() default "";
+
+    /**
+     * Use this to have this method only called when the focused option of an interaction event.
+     * @see net.dv8tion.jda.api.events.interaction.component.StringSelectInteractionEvent
+     * @see net.dv8tion.jda.api.events.interaction.command.CommandAutoCompleteInteractionEvent
+     * @return focused option of the command you would like to match it with
+     */
     String FocusedOption() default "";
 }
